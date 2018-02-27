3fccc84774ca51f39f319ddbaa63142ab3b69a40
不完美的艺术 <xin963744135@163.com>
2018-02-27 15:13:08 +0800

    更新支付宝SDK 15.5.0

--- AliSDKDemo/AliSDKDemo/APWebViewController.m
+++ AliSDKDemo/AliSDKDemo/APWebViewController.m
@@ -2,8 +2,8 @@
 //  APWebViewController.m
 //  AliSDKDemo
 //
-//  Created by 亦澄 on 16-1-5.
-//  Copyright (c) 2016年 Alipay. All rights reserved.
+//  Created by antfin on 17-10-24.
+//  Copyright (c) 2017年 AntFin. All rights reserved.
 //
 
 #import "APWebViewController.h"
@@ -29,6 +29,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    self.view.backgroundColor = [UIColor whiteColor];
     self.webView.delegate = self;
 
     // 加载已经配置的url
@@ -44,12 +45,23 @@
 
 - (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType
 {
-    NSString* orderInfo = [[AlipaySDK defaultService]fetchOrderInfoFromH5PayUrl:[request.URL absoluteString]];
-    if (orderInfo.length > 0) {
-        [self payWithUrlOrder:orderInfo];
+    //新版本的H5拦截支付对老版本的获取订单串和订单支付接口进行合并，推荐使用该接口
+    __weak APWebViewController* wself = self;
+    BOOL isIntercepted = [[AlipaySDK defaultService] payInterceptorWithUrl:[request.URL absoluteString] fromScheme:@"alisdkdemo" callback:^(NSDictionary *result) {
+        // 处理支付结果
+        NSLog(@"%@", result);
+        // isProcessUrlPay 代表 支付宝已经处理该URL
+        if ([result[@"isProcessUrlPay"] boolValue]) {
+            // returnUrl 代表 第三方App需要跳转的成功页URL
+            NSString* urlStr = result[@"returnUrl"];
+            [wself loadWithUrlStr:urlStr];
+        }
+    }];
+    
+    if (isIntercepted) {
         return NO;
     }
-	return YES;
+    return YES;
 }
 
 - (void)loadWithUrlStr:(NSString*)urlStr
@@ -61,27 +73,6 @@
                                                     timeoutInterval:30];
             [self.webView loadRequest:webRequest];
         });
-    }
-}
-
-
-#pragma mark -
-#pragma mark   ============== URL pay 开始支付 ==============
-
-- (void)payWithUrlOrder:(NSString*)urlOrder
-{
-    if (urlOrder.length > 0) {
-        __weak APWebViewController* wself = self;
-        [[AlipaySDK defaultService]payUrlOrder:urlOrder fromScheme:@"alisdkdemo" callback:^(NSDictionary* result) {
-            // 处理支付结果
-            NSLog(@"%@", result);
-            // isProcessUrlPay 代表 支付宝已经处理该URL
-            if ([result[@"isProcessUrlPay"] boolValue]) {
-                // returnUrl 代表 第三方App需要跳转的成功页URL
-                NSString* urlStr = result[@"returnUrl"];
-                [wself loadWithUrlStr:urlStr];
-            }
-        }];
     }
 }
 
@@ -155,7 +146,7 @@
     NSString* urlStr = self.urlInput.text;
     if (urlStr.length > 0) {
         if (![urlStr hasPrefix:@"http"]) {
-            urlStr = [NSString stringWithFormat:@"http://%@", urlStr];
+            urlStr = [NSString stringWithFormat:@"https://%@", urlStr];
         }
         [[NSUserDefaults standardUserDefaults] setObject:urlStr forKey:@"alipayweburl"];
         [self loadWithUrlStr:urlStr];
